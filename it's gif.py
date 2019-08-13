b3522f04f14dca415f23938b00032c4da4b921e5
DiscoOstrich21 <cameron.kunstadt@gmail.com>
2019-08-12 21:26:14 -0600

    Update it's gif.py

--- it's gif.py
+++ it's gif.py
@@ -1,4 +1,4 @@
-#Cameron Kunstadt
+# Cameron Kunstadt
 # 8/1/2019
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -8,25 +8,26 @@ definitionLinks = []  # list of all the links
 gCounter = 0  # counting up words with the 'g' pronunciation
 jCounter = 0  # counting up words with the 'j' pronunciation
 
-def PronunciationGrabber(individualDefiniton):
+
+def pronunciation_grabber(individual_definition):
     #  this method opens up the individual definition page link, finds the pronunciation guide, and determines
     #  which sound it has, and increments its respective counter
-    if " " not in individualDefiniton.attrs['href']:
+    if " " not in individual_definition.attrs['href']:
         #  definitions with spaces have different urls, and most often do not have pron. guides, and can be ignored
-        if "\\" in str(individualDefiniton.encode('utf-8')):
+        if "\\" in str(individual_definition.encode('utf-8')):
             # if, when converted to unicode, the word contains a '\', it is a special non-ASCII character
             # and for these purposes can be be ignored
             print("Unreadable Character")
             return 0
-        print(individualDefiniton)
-        defPage = urlopen("https://www.merriam-webster.com" + str(individualDefiniton.attrs['href']))
-        pageObj = BeautifulSoup(defPage, "html.parser")
-        print(pageObj.find("span", {"class": "pr"}))
-        if pageObj.find("span", {"class": "pr"}) is not None:
-            encodedPronun = (str(pageObj.find("span", {"class": "pr"})))
-            if 'j' in encodedPronun:
+        print(individual_definition)
+        def_page = urlopen("https://www.merriam-webster.com" + str(individual_definition.attrs['href']))
+        page_obj = BeautifulSoup(def_page, "html.parser")
+        print(page_obj.find("span", {"class": "pr"}))
+        if page_obj.find("span", {"class": "pr"}) is not None:
+            pronunciation = (str(page_obj.find("span", {"class": "pr"})))
+            if 'j' in pronunciation:
                 return 2
-            if 'g' in encodedPronun:
+            if 'g' in pronunciation:
                 return 1
         else:
             print("NO IPA")
@@ -36,36 +37,35 @@ def PronunciationGrabber(individualDefiniton):
         return 0
 
 
-def GetLocalLinks(currentSite):
+def get_local_links(current_site):
     # gets all the links on the page, adds them to list
-    html = urlopen(currentSite)
-    bsObj = BeautifulSoup(html, "html.parser")
-    for link in bsObj.findAll("a", href=re.compile("^(/dictionary/)((?!:).)*$")):
+    html = urlopen(current_site)
+    bs_obj = BeautifulSoup(html, "html.parser")
+    for link in bs_obj.findAll("a", href=re.compile("^(/dictionary/)((?!:).)*$")):
             definitionLinks.append(link)
             print(link)
 
 
-def PageShifter(startingSite):
-    ourPage = (startingSite + "/")
-    #setting up the link to accept a num at the end
+def page_shifter(starting_site):
+    our_page = (starting_site + "/")
+    # setting up the link to accept a num at the end
     for pageNum in range(1, 44):
-        ourPage = ourPage+str(pageNum)
-        #adding the num at the end to turn pages
-        print(ourPage)
-        GetLocalLinks(ourPage)
-        #grabbing the links on every page
-        ourPage = (startingSite + "/")
-        #setting it back to take the number off
+        our_page = our_page+str(pageNum)
+        # adding the num at the end to turn pages
+        print(our_page)
+        get_local_links(our_page)
+        # grabbing the links on every page
+        our_page = (starting_site + "/")
+        # setting it back to take the number off
 
 
-
-PageShifter("https://www.merriam-webster.com/browse/dictionary/g")
+page_shifter("https://www.merriam-webster.com/browse/dictionary/g")
 
 for links in definitionLinks[0:]:
-    if PronunciationGrabber(links) == 1:
+    if pronunciation_grabber(links) == 1:
         gCounter += 1
         print("gCounter is: " + str(gCounter))
-    if PronunciationGrabber(links) == 2:
+    if pronunciation_grabber(links) == 2:
         jCounter += 1
         print("jCounter is: " + str(jCounter))
 
@@ -76,4 +76,5 @@ if gCounter > jCounter:
     print("SEE, ITS GIF")
 else:
     print("ok fine, i guess it's jif")
+
 
